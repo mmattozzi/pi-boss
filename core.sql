eb1e998eca9edfa87cc5da5857c90bc393ec4360
Mike Mattozzi <mike.mattozzi@gmail.com>
2017-02-04 22:13:28 -0500

    Updating SQL so it creates table in new user space

--- core.sql
+++ core.sql
@@ -4,11 +4,11 @@ create database pi_boss;
 create user 'pi_boss'@'localhost';
 grant all privileges on pi_boss.* TO 'pi_boss'@'localhost';
 
-create table numeric_log ( 
+create table pi_boss.numeric_log ( 
     host varchar(20),
     value float,
     function varchar(64),
     date timestamp default CURRENT_TIMESTAMP
 );
 
-create index func_timestamp_idx on numeric_log (function, date);
+create index func_timestamp_idx on pi_boss.numeric_log (function, date);
